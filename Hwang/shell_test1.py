184a62e19296d2b7c6c156763262eb298fa9db4b
Byung-Hak Hwang <xoda90@gmail.com>
2022-06-11 22:38:48 +0900

    Add shell_test

---
+++ Hwang/shell_test1.py
@@ -0,0 +1,5 @@
+import time
+
+print("TEST1 Start!")
+time.sleep(5)
+print("TEST1 End!!!!")
---
+++ Hwang/shell_test2.py
@@ -0,0 +1,7 @@
+import time
+
+print("TEST2 Start!")
+time.sleep(5)
+for i in range(4):
+    print(f'{i} and {i*i}')
+print("TEST2 End!!!!")
