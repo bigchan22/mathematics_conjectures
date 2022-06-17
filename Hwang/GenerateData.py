bed9af76db9ee51f4d4faee340f55b03efc01457
Byung-Hak Hwang <xoda90@gmail.com>
2022-06-17 14:46:30 +0900

    Minor correction

--- Hwang/GenerateData.py
+++ Hwang/GenerateData.py
@@ -2,4 +2,4 @@ import generate_data as gen
 
 DIR_PATH = '/root/Hwang/mathematics_conjectures/Hwang/Data/'
 !mkdir {DIR_PATH}
-gen.generate_data(DIR_PATH=DIR_PATH, N=7, primitive = True, connected=False, extended=False, UPTO_N=False)
+gen.generate_data(DIR_PATH=DIR_PATH, input_N=7, primitive=True, connected=False, extended=False, UPTO_N=False)
