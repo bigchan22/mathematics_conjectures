86c091b01741e2139f40f644ad97f1a72eb67a71
Byung-Hak Hwang <xoda90@gmail.com>
2022-06-18 22:57:31 +0900

    Minor correction

--- Hwang/utils_modified.py
+++ Hwang/utils_modified.py
@@ -29,59 +29,6 @@ import seaborn as sns
 
 from training_info import *
 
-def random_feature(D):
-    rand_feature = dict.fromkeys(D.nodes)
-    for key in rand_feature.keys():
-        rand_feature[key] = np.random.rand()
-    return rand_feature
-
-def constant_feature(D):
-    const_feature = dict.fromkeys(D.nodes)
-    for key in const_feature.keys():
-        const_feature[key] = 1
-    return const_feature
-
-def numbering_feature(D):
-    num_feature = dict()
-    for n, node in enumerate(D.nodes):
-        num_feature[node] = n
-    return num_feature
-
-def get_sinks(D):
-    return (node for node, out_dg in D.out_degree() if out_dg == 0)
-
-
-def shortest_path_lengths(D):
-    sinks = get_sinks(D)
-    shortest_lengths = dict.fromkeys(D.nodes, float('inf'))
-    for sink in sinks:
-        for node, length in nx.shortest_path_length(D, target=sink).items():
-            if shortest_lengths[node] > length:
-                shortest_lengths[node] = length
-    return shortest_lengths
-
-
-def longest_path_length_to_target(D, target):
-    dist = dict.fromkeys(D.nodes, -float('inf'))
-    dist[target] = 0
-    topo_order = reversed(list(nx.topological_sort(D)))
-    for v in topo_order:
-        for u in D.predecessors(v):
-            if dist[u] < dist[v] + 1:
-                dist[u] = dist[v] + 1
-    return dist
-
-
-def longest_path_lengths(D):
-    sinks = get_sinks(D)
-    longest_lengths = dict.fromkeys(D.nodes, -float('inf'))
-    for sink in sinks:
-        for node, length in longest_path_length_to_target(D, target=sink).items():
-            if longest_lengths[node] < length:
-                longest_lengths[node] = length
-    return longest_lengths
-
-
 def convert_networkx_to_adjacency_input(graph):
     adjacency_matrix = nx.to_scipy_sparse_array(graph, format='coo')
     adjacency_matrix += sp.eye(adjacency_matrix.shape[0])
