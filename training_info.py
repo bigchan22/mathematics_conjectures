7c447ffda2831f6b2c1a63413e1ce0c5a0bfc09f
Byung-Hak Hwang <xoda90@gmail.com>
2022-07-22 23:21:44 +0900

    Minor correction

--- training_info.py
+++ training_info.py
@@ -26,10 +26,10 @@ step_size = 0.001
 train_fraction = .8
 
 DIR_PATH = '/Data/mathematics_conjectures'
-GRAPH_DIR = os.path.join(DIR_PATH, f'Data/N_{N}')
+GRAPH_DIR = os.path.join(DIR_PATH, f'Hwang/Data/N_{N}')
 PARAM_DIR = os.path.join(DIR_PATH, 'Parameters')
 NUM_GRAPHS = len([f for f in os.listdir(GRAPH_DIR) if f.startswith("graph_")])
-PARAM_FILE = os.path.join(PARAM_DIR, f'parameters_{N}_{partition_part}_{num_layers}_{num_features}')
+PARAM_FILE = os.path.join(PARAM_DIR, f'parameters_{N}_{partition_parts}_{num_layers}_{num_features}')
 for key in feature_list.keys():
     PARAM_FILE += f'_{key}'
-PARAM_FILE += '_v2.pickle'
+PARAM_FILE += '.pickle'
